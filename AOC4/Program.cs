35e4075cde0a82a5d01e9e9e35122ba68b5d7d54
christoffer.fjellstrom <christoffer.fjellstrom@qlok.se>
2023-12-29 19:51:32 +0100

    Add project files.

---
+++ AOC4/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace AOC4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int score = 0;
+            string[] lines = File.ReadAllLines("input.txt");
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(": ");
+                string[] numStrings = parts[1].Split(" | ");
+
+                var winners = numStrings[0]
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToHashSet();
+
+                var myNums = numStrings[1]
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse);
+
+                int nextScore = 1;
+                int setScore = 0;
+
+                foreach(int num in myNums.Where(winners.Contains))
+                {
+                    setScore = nextScore;
+                    nextScore *= 2;
+                }
+                score += setScore;
+            }
+            Console.WriteLine(score);
+        }
+    }
+}
---
+++ Part2/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Part2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("input.txt");
+            var counts = Enumerable.Repeat(1, lines.Length).ToArray();
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                var line = lines[i];
+                string[] parts = line.Split(": ");
+                string[] numStrings = parts[1].Split(" | ");
+
+                var winners = numStrings[0]
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToHashSet();
+
+                int score = numStrings[1]
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .Count(winners.Contains);
+
+                for (int j = 1; j <= score; j++)
+                {
+                    counts[i + j] += counts[i];
+                }
+            }
+
+            Console.WriteLine(counts.Sum());
+        }
+    }
+}
